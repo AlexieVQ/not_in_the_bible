7b94c202f193b6b841f70d6642707c6222c2bd37
AlexieVQ <alexie@nephos.me>
2022-01-15 22:22:31 +0100

    Changed reference to owned

--- src/response.rs
+++ src/response.rs
@@ -2,20 +2,20 @@ use crate::request::Request;
 
 /// A response to a Request
 pub struct Response {
-    request: &Request,
+    request: Request,
     body: String,
 }
 
 impl Response {
 
     /// Creates a new Response for given response with given body.
-    fn new(request: &Request, body: String) -> Response{
+    fn new(request: Request, body: String) -> Response{
         Response { request, body }
     }
 
     /// The Request to which this response answers.
     fn response(&self) -> &Request {
-        self.request
+        &self.request
     }
 
     /// The response's body (without usernames).
