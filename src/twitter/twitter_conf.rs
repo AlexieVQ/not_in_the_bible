f868ffa23bdded048ee1ef04b66d8159c09aeb3d
AlexieVQ <alexie@nephos.me>
2022-01-19 21:57:57 +0100

    Added tweet limits to twitter conf

--- src/twitter/twitter_conf.rs
+++ src/twitter/twitter_conf.rs
@@ -1,9 +1,17 @@
 use yaml_rust::Yaml;
 
+/// Default maximum number of tweets sent in one hour
+const UDPATES_PER_HOUR: i64 = 12;
+
+/// Default refresh interval (in seconds)
+const REFRESH_INTERVAL: i64 = 60;
+
 /// Configuration for the Twitter API.
 pub struct TwitterConf {
     pub api_key: String,
     pub api_secret: String,
+    pub updates_per_hour: i64,
+    pub refresh_interval: i64,
 }
 
 impl TwitterConf {
@@ -18,7 +26,15 @@ impl TwitterConf {
             api_secret: yaml["api_secret"]
                 .as_str()
                 .expect("Missing or wrong twitter api_secret")
-                .to_string()
+                .to_string(),
+            updates_per_hour: match yaml["updates_per_hour"] {
+                Yaml::Integer(i) => i,
+                _ => UDPATES_PER_HOUR,
+            },
+            refresh_interval: match yaml["refresh_interval"] {
+                Yaml::Integer(i) => i,
+                _ => REFRESH_INTERVAL,
+            },
         }
     }
 
