e4152d6f65a2b9a99262975146e546f6d02d1d2d
AlexieVQ <alexie@nephos.me>
2022-01-21 23:00:15 +0100

    Added methods to Connection

--- src/twitter/connection.rs
+++ src/twitter/connection.rs
@@ -4,7 +4,7 @@ use oauth_client::{DefaultRequestBuilder, Token, Error};
 use reqwest::blocking::Client;
 use serde::{Deserialize, de::DeserializeOwned};
 
-use super::twitter_conf::TwitterConf;
+use super::{twitter_conf::TwitterConf, tweet::Tweet};
 
 const TWITTER_API_REQUEST_TOKEN_URL: &str =
     "https://api.twitter.com/oauth/request_token";
@@ -14,6 +14,15 @@ const TWITTER_API_AUTHORIZE_URL: &str =
 
 const TWITTER_API_ACCESS_TOKEN_URL: &str =
     "https://api.twitter.com/oauth/access_token";
+
+const TWITTER_API_MENTIONS_TIMELINE_URL: &str =
+    "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
+
+const TWITTER_API_TWEET_SHOW_URL: &str =
+    "https://api.twitter.com/1.1/statuses/show.json";
+
+const TWITTER_API_STATUSES_UPDATE_URL: &str =
+    "https://api.twitter.com/1.1/statuses/update.json";
 
 /// A connection to a Twitter account.
 pub struct Connection<'a> {
@@ -82,10 +91,10 @@ impl <'a> Connection<'a> {
     pub fn get<T: DeserializeOwned>(
         &self,
         url: &str,
-        body: HashMap<Cow<'a, str>, Cow<'a, str>>) -> Result<T, Error<DefaultRequestBuilder>> {
+        query: HashMap<Cow<'a, str>, Cow<'a, str>>) -> Result<T, Error<DefaultRequestBuilder>> {
         match oauth_client::get::<DefaultRequestBuilder>(
             &url.to_string(), &self.consumer, Some(&self.token),
-            Some(&body), &()) {
+            Some(&query), &()) {
             Ok(string) => {
                 let object: T = serde_json::from_str(&string)
                     .expect("Malformed JSON data");
@@ -110,6 +119,43 @@ impl <'a> Connection<'a> {
             },
             Err(error) => Err(error),
         }
+    }
+
+    /// Returns mentions since a given id (latest first).
+    pub fn mentions(&self, since: Option<&str>) -> Vec<Tweet> {
+        let mut param: HashMap<Cow<'a, str>, Cow<'a, str>> = HashMap::new();
+        if let Some(since) = since {
+            param.insert(Cow::Borrowed("since_id"),
+                Cow::Owned(since.to_string()));
+        }
+        param.insert(Cow::Borrowed("count"), Cow::Borrowed("200"));
+        self.get(TWITTER_API_MENTIONS_TIMELINE_URL, param)
+            .unwrap_or(vec![])
+    }
+
+    /// Returns a tweet by its id.
+    pub fn by_id(&self, id: &str) -> Result<Tweet, Error<DefaultRequestBuilder>>
+    {
+        let mut param: HashMap<Cow<'a, str>, Cow<'a, str>> = HashMap::new();
+        param.insert(Cow::Borrowed("id"), Cow::Owned(id.to_string()));
+        param.insert(Cow::Borrowed("include_entities"),
+            Cow::Borrowed("false"));
+        self.get(TWITTER_API_TWEET_SHOW_URL, param)
+    }
+
+    /// Replies to tweet of given id (@username is automatically added to body).
+    pub fn reply(&self, to_id: &str, to_users: &[&str], body: &str) ->
+        Result<Tweet, Error<DefaultRequestBuilder>> {
+        let mut param: HashMap<Cow<'a, str>, Cow<'a, str>> = HashMap::new();
+        param.insert(Cow::Borrowed("status"),
+            Cow::Owned(format!("{} {}", to_users
+            .iter()
+            .map(|u| format!("@{}", u))
+            .collect::<Vec<String>>()
+            .join(" "), body)));
+        param.insert(Cow::Borrowed("in_reply_to_status_id"),
+            Cow::Owned(to_id.to_string()));
+        self.post(TWITTER_API_STATUSES_UPDATE_URL, param)
     }
 
 }
